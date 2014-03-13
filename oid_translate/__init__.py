1e3c7afd9f2edc84fac0fce1de1959d12583186e
Gary M. Josack <gary@byoteki.com>
2014-03-12 20:48:08 -0700

    New Features and Cleanup
    This change breaks backwards compatibility.
    
     * Renamed some functions to be more clear
     * Added ObjectId abstraction for higher level interface
     * Added ability to pull hint, textual, and enums for an OID
     * Various functions now raise value error for invalid OID

--- oid_translate/__init__.py
+++ oid_translate/__init__.py
@@ -9,11 +9,56 @@ OID_OUTPUT_UCD = 5
 OID_OUTPUT_NONE = 6
 
 
-def init_mibs():
+def load_mibs():
+    """ Preload MIB files into tree.
+
+    This isn't necessary, as MIBs will be loaded lazily the first time
+    a translation is requested. This will preload the MIB files ahead
+    of time.
+    """
     _oid_translate.init()
 
 
 def translate(oid, output_type=OID_OUTPUT_DEFAULT):
-    if oid.endswith("."):
-        oid = oid[:-1]
     return _oid_translate.translate(oid, output_type)
+
+
+def get_details(oid):
+    return _oid_translate.get_details(oid)
+
+
+class ObjectId(object):
+    def __init__(self, objid):
+        # Do a translation now to find bad values early.
+        self.oid = translate(objid, OID_OUTPUT_NUMERIC)
+        self._details = None
+
+    @property
+    def name(self):
+        return translate(self.oid, OID_OUTPUT_DEFAULT)
+
+    @property
+    def suffix(self):
+        return translate(self.oid, OID_OUTPUT_SUFFIX)
+
+    @property
+    def fullname(self):
+        return translate(self.oid, OID_OUTPUT_FULL)
+
+    @property
+    def hint(self):
+        if self._details is None:
+            self._details = get_details(self.oid)
+        return self._details.get("hint")
+
+    @property
+    def enums(self):
+        if self._details is None:
+            self._details = get_details(self.oid)
+        return self._details.get("enums", {})
+
+    @property
+    def textual(self):
+        if self._details is None:
+            self._details = get_details(self.oid)
+        return self._details.get("textual")
--- oid_translate/_oid_translate.c
+++ oid_translate/_oid_translate.c
@@ -10,34 +10,9 @@
 
 static int initialized = 0;
 
-u_char *
-oid_to_name(char *objid, int output_type)
-{
-    u_char *buf = NULL;
-    size_t  objidlen;
-    size_t  buf_len = 256;
-    size_t  out_len = 0;
-    int     buf_overflow = 0;
-    oid     name[MAX_OID_LEN];
 
-    netsnmp_ds_set_int(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_OID_OUTPUT_FORMAT, output_type);
-
-    objidlen = MAX_OID_LEN;
-    if (!read_objid(objid, name, &objidlen)) {
-        snmp_perror(objid);
-        exit(2);
-    }
-
-    buf = (u_char *) calloc(buf_len, 1);
-    netsnmp_sprint_realloc_objid_tree(&buf, &buf_len, &out_len, 1,
-                                      &buf_overflow, name, objidlen);
-
-    return buf;
-}
-
-
-static PyObject *
-oid_translate_init(PyObject *self, PyObject *args)
+static void
+_init_snmp(void)
 {
     int priority = LOG_DEBUG;
     int pri_max  = LOG_EMERG;
@@ -49,6 +24,37 @@ oid_translate_init(PyObject *self, PyObject *args)
 
     init_snmp("oid_translate");
     initialized = 1;
+}
+
+
+static u_char *
+objid_to_name(char *objid, int output_type)
+{
+    u_char *buf = NULL;
+    size_t  buf_len = 256;
+    size_t  out_len = 0;
+    int     buf_overflow = 0;
+    oid     oid[MAX_OID_LEN];
+    size_t  objidlen = MAX_OID_LEN;
+
+    if (!read_objid(objid, oid, &objidlen)) {
+        snmp_perror(objid);
+        return NULL;
+    }
+
+    netsnmp_ds_set_int(NETSNMP_DS_LIBRARY_ID, NETSNMP_DS_LIB_OID_OUTPUT_FORMAT, output_type);
+    buf = (u_char *) calloc(buf_len, 1);
+    netsnmp_sprint_realloc_objid_tree(&buf, &buf_len, &out_len, 1,
+                                      &buf_overflow, oid, objidlen);
+
+    return buf;
+}
+
+
+static PyObject *
+oid_translate_init(PyObject *self, PyObject *args)
+{
+    _init_snmp();
     Py_RETURN_NONE;
 }
 
@@ -56,29 +62,79 @@ oid_translate_init(PyObject *self, PyObject *args)
 static PyObject *
 oid_translate_translate(PyObject *self, PyObject *args)
 {
-    char *oid;
-    u_char     *output;
-    int         output_type;
-    PyObject   *out;
+    char     *objid;
+    int       output_type;
 
-    if (!PyArg_ParseTuple(args, "si", &oid, &output_type))
+    u_char   *name;
+    PyObject *out;
+
+    if (!PyArg_ParseTuple(args, "si", &objid, &output_type))
         return NULL;
 
-    if (!initialized){
-        netsnmp_init_mib();
-        initialized = 1;
+    if (!initialized) _init_snmp();
+
+    name = objid_to_name(objid, output_type);
+    if (!name){
+        PyErr_SetString(PyExc_ValueError, snmp_api_errstring(snmp_errno));
+        return NULL;
     }
 
-    output = oid_to_name(oid, output_type);
-    out = Py_BuildValue("s", output);
-    SNMP_FREE(output);
+    out = Py_BuildValue("s", name);
+    SNMP_FREE(name);
     return out;
+}
+
+
+static PyObject *
+oid_translate_get_details(PyObject *self, PyObject *args)
+{
+    char        *objid;
+    struct tree *tp;
+    oid          oid[MAX_OID_LEN];
+    size_t       objidlen;
+
+    if (!PyArg_ParseTuple(args, "s", &objid))
+        return NULL;
+
+    if (!initialized) _init_snmp();
+
+    objidlen = MAX_OID_LEN;
+    if (!read_objid(objid, oid, &objidlen)) {
+        PyErr_SetString(PyExc_ValueError, snmp_api_errstring(snmp_errno));
+        return NULL;
+    }
+
+    PyObject *outDict = PyDict_New();
+    PyObject *enums = PyDict_New();
+
+    tp = get_tree(oid, objidlen, get_tree_head());
+
+    PyDict_SetItemString(outDict, "textual",
+        Py_BuildValue("z", get_tc_descriptor(tp->tc_index))
+    );
+    PyDict_SetItemString(outDict, "hint",
+        Py_BuildValue("z", tp->hint)
+    );
+
+    if (tp->enums){
+        struct enum_list *ep = tp->enums;
+        while(ep){
+            PyDict_SetItem(enums,
+                Py_BuildValue("i", ep->value),
+                Py_BuildValue("s", ep->label)
+            );
+            ep = ep->next;
+        }
+    }
+    PyDict_SetItemString(outDict, "enums", enums);
+    return outDict;
 }
 
 
 static PyMethodDef OidTranslateMethods[] = {
     {"init",  oid_translate_init, METH_VARARGS, "Initialize MIB data."},
     {"translate",  oid_translate_translate, METH_VARARGS, "Translate OID to Name or vice versa."},
+    {"get_details",  oid_translate_get_details, METH_VARARGS, "Get extra details for OID."},
     {NULL, NULL, 0, NULL},
 };
 
